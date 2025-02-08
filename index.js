5d46dd1b50bca297a429c44e070c9612e656dc23
Brandon Hubbard <bhubbard@users.noreply.github.com>
2025-02-08 11:35:38 -0800

    Setup .env to manage access token

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+// console.log(process.env);
+
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -7,12 +11,18 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+
+// Get Token from config file.
+const HUBSPOT_ACCESS_KEY = process.env.HUBSPOT_ACCESS_KEY;
+
+console.log(HUBSPOT_ACCESS_KEY);
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+// Print Hello World
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
